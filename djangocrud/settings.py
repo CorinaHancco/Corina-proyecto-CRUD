68a3d39123f57e24f77231f9e2cdba7f67603930
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-31 18:04:53 -0500

    Listado peliculas desde django en angular

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#CORS_ORIGIN_WHITELIST = (
+ #   'localhost:62887'
+#)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:62887",
+]
+
 
 # Application definition
 
@@ -38,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Appi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
