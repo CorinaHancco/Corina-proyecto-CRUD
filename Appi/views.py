25a18feb19e140449e42f2dd98c4a5da1c5cbd97
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-31 17:12:02 -0500

    vista implementada

--- Appi/views.py
+++ Appi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import MovieSerializer
+from .models import Movie
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+   
+
